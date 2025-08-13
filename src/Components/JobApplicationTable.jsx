d9dd5f0e035f6819e595e104700e870407675ec0
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-08-13 18:41:06 +0600

    resolve

--- src/Components/JobApplicationTable.jsx
+++ src/Components/JobApplicationTable.jsx
@@ -16,17 +16,13 @@ const JobApplicationTable = ({ application, index }) => {
             </div>
           </div>
           <div>
-            <div className="font-bold">Hart Hagerty</div>
-            <div className="text-sm opacity-50">United States</div>
+            <div className="font-bold">{title}</div>
           </div>
         </div>
       </td>
       <td>
-        Zemlak, Daniel and Leannon
+        {company}
         <br />
-        <span className="badge badge-ghost badge-sm">
-          Desktop Support Technician
-        </span>
       </td>
       <td>Purple</td>
       <th>
